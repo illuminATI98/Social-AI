fc4b6ec16c09c4034e4aa5c0442437a12ae4776d
Attila Dobi <attiladobi100@gmail.com>
2023-05-30 18:17:40 +0200

    Add page Home

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+
+
+  return (
+    <div className='home-container'>
+      <div className='home-posts'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Home
