14ddbc0790bdf7e4eda472c81aec41ced1e5732e
Attila Dobi <attiladobi100@gmail.com>
2023-06-07 00:02:27 +0200

    Create Post layout

---
+++ client/src/components/Post/Post.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Post = () => {
+  return (
+    <div className='post-container'>
+      <div className='post-name'></div>
+      <div className='post-image'></div>
+      <div className='post-interactions'></div>
+      <div className='post-likes'></div>
+      <div className='post-description'></div>
+      <div className='post-comments'></div>
+    </div>
+  )
+}
+
+export default Post
