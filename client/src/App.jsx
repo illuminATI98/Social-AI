b6b14d05788476727dfc3d891c8622e86caa8a63
Attila Dobi <attiladobi100@gmail.com>
2023-06-07 00:00:26 +0200

    Add Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
-
-import './App.css'
+import React, { useState, useEffect  } from 'react';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { Home, CreatePost } from './pages';
+import { Navbar } from './components';
+import './App.css';
 
 function App() {
-  
+  const [loading, setloading] = useState(false);
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <div className='main-container'>
+        <Navbar/>
+        <div className="page">
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/create-post" element={<CreatePost/>}/>
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
