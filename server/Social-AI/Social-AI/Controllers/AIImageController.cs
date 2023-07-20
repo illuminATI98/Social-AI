8f4b6000d1ca1965b8a372f9ad5cfa136d37016c
Attila Dobi <attiladobi100@gmail.com>
2023-07-20 17:13:33 +0200

    Add AIImageController.cs and intall OpenAI nugets

---
+++ server/Social-AI/Social-AI/Controllers/AIImageController.cs
@@ -0,0 +1,26 @@
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using OpenAI;
+using OpenAI.Images;
+
+namespace Social_AI.Controllers;
+
+public class AIImageController : ControllerBase
+{
+    string APIKEY = string.Empty;
+    
+    public AIImageController(IConfiguration conf)
+    {
+        APIKEY = conf.GetSection("OPENAI_API_KEY").Value;
+    }
+    
+    [Route("GenerateAIImage")]
+    [HttpPost]
+    public async Task<IActionResult> GenerateAIImage([FromBody] string prompt)
+    {
+        
+        var _openAIClient = new OpenAIClient(new OpenAIAuthentication(APIKEY));
+        var result = await _openAIClient.ImagesEndPoint.GenerateImageAsync(prompt, 1, ImageSize.Large);
+        return Ok(result);
+    }
+}
