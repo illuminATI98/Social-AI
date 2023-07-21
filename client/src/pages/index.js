efd0bedfd0106e45e7d4e043b160269367d5dcdb
Attila Dobi <attiladobi100@gmail.com>
2023-07-21 05:14:08 +0200

    Add login and register to export

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,7 +1,11 @@
 import Home from "./Home/Home";
 import CreatePost from "./CreatePost/CreatePost";
+import Login from "./Login/Login";
+import Register from "./Register/Register";
 
 export {
   Home,
-  CreatePost
+  CreatePost,
+  Login,
+  Register
 }
