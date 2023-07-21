e0fcb8707ca930e941042619f8f53f2be37b0675
Attila Dobi <attiladobi100@gmail.com>
2023-07-21 05:13:19 +0200

    Create register page

---
+++ client/src/pages/Register/Register.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    name:'',
+    password: ''
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Here, you can make an API call to your backend authentication endpoint
+    // Send 'formData.username' and 'formData.password' to authenticate the user
+    // Upon successful login, you can store the received JWT token in the browser's localStorage or sessionStorage
+
+    // For demo purposes, let's just log the form data
+    console.log(formData);
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
