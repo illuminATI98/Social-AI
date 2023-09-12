361d67cf20eac1be08562996b9e7836dc6701c30
Attila Dobi <attiladobi100@gmail.com>
2023-09-12 13:45:18 +0200

    Create IUserService Interface

---
+++ server/Social-AI/Social-AI/Services/Interfaces/IUserService.cs
@@ -0,0 +1,19 @@
+using Social_AI.Models.Entities;
+
+namespace Social_AI.Services;
+
+public interface IUserService 
+{
+    public Task Add(User entity);
+    
+    public Task<User> Get(long id);
+    
+    public Task<IEnumerable<User>> GetAll();
+    
+    public Task Update(User entity);
+    
+    public Task Delete(long id);
+    bool UserExistsByEmail(string email);
+    Task<User> GetByLogin(string email, string password);
+    bool UserExistsById(long id);
+}
