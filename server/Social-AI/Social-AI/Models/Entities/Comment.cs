e3d7bf5838e719aaf945c6c6a42448a6de6876d1
Attila Dobi <attiladobi100@gmail.com>
2023-09-12 13:44:27 +0200

    Add time, user and post Id to Comment entity

--- server/Social-AI/Social-AI/Models/Entities/Comment.cs
+++ server/Social-AI/Social-AI/Models/Entities/Comment.cs
@@ -9,4 +9,14 @@ public class Comment
 
     public string Text { get; set; }
     
+    public DateTime CreatedAt { get; set; }
+
+    public int UserId { get; set; } 
+    
+    public User User { get; set; } 
+    
+    public int PostId { get; set; } 
+    
+    public Post Post { get; set; }
+    
 }
