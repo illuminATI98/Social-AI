e752e3febd0d825a8ae0f88b434d459e24da4ab8
Attila Dobi <attiladobi100@gmail.com>
2023-09-12 13:48:28 +0200

    Add Likes and Comments to DB

--- server/Social-AI/Social-AI/SocialAiContext.cs
+++ server/Social-AI/Social-AI/SocialAiContext.cs
@@ -9,6 +9,10 @@ public class SocialAiContext : DbContext
 
     public DbSet<Post> Posts { get; set; }
 
+    public DbSet<Comment> Comments { get; set; }
+
+    public DbSet<Like> Likes { get; set; }
+
     public SocialAiContext(DbContextOptions<SocialAiContext> options) : base(options)
     {
         
