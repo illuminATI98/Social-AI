1c75564601dbc3abfb9b0a6f33ab4994045ecd40
Attila Dobi <attiladobi100@gmail.com>
2023-09-13 12:08:56 +0200

    Create CreatePostDTO.cs

---
+++ server/Social-AI/Social-AI/Models/DTOs/CreatePostDTO.cs
@@ -0,0 +1,10 @@
+namespace Social_AI.Models.DTOs;
+
+public class CreatePostDTO
+{
+    public string Prompt { get; set; }
+
+    public string Image { get; set; }
+    
+    public string Description { get; set; }
+}
