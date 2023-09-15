886c7c16fa784fe864a58fb00e2bc663b59ec09a
Attila Dobi <attiladobi100@gmail.com>
2023-09-15 09:54:49 +0200

    Add getAllPosts method, Render all of them

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,17 +1,65 @@
-import React, { useState } from 'react';
-import { Post } from '../../components';
+import React, { useState, useEffect } from 'react';
+import { BACKEND_API_URL } from '../../url';
+import jwt_decode from "jwt-decode";
+import Cookies from 'js-cookie';
+import { PostCard } from '../../components';
 
 const Home = () => {
-  const [loading, setloading] = useState(false);
-  const [allPosts, setallPosts] = useState(null);
+  const [allPosts, setAllPosts] = useState(null);
+  const [user, setUser] = useState(null);
+  const [jwtToken, setJwtToken] = useState(null);
 
+  useEffect(() => {
+    try {
+      const token = Cookies.get("jwtToken");
+      if (token) {
+        const decodedToken = jwt_decode(token);
+        setUser(decodedToken);
+        setJwtToken(token);
+      }
+    } catch (error) {
+      console.error("Error decoding token:", error);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (jwtToken) {
+      async function getAllPosts() {
+        try {
+          const response = await fetch(`${BACKEND_API_URL}/api/posts`, {
+            method: "GET",
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${jwtToken}`
+            },
+          });
+          const data = await response.json();
+          setAllPosts(data["$values"]);
+        } catch (error) {
+          alert(error);
+        }
+      }
+
+      getAllPosts();
+    }
+  }, [jwtToken]);
+  
   return (
     <div className='home-container'>
       <div className='home-posts'>
-        Home
+        {allPosts === null ? (
+          <div>Loading...</div>
+        ) : (
+          <div>
+            {allPosts.map(post => (
+              <PostCard key={post.id} postData={post} />
+            ))}
+          </div>
+        )}
       </div>
     </div>
-  )
+  );
 }
 
 export default Home
